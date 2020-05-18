91bdf4c6f13d3891f77cd25bcda16087b241bb60
Anderson Matuchenko <anderson.matuchenko@buonny.com.br>
2020-05-18 17:22:57 -0300

    Criação de Classe Posicao

--- XadrezConsole/Program.cs
+++ XadrezConsole/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using XadrezConsole.Tabuleiro;
 
 namespace XadrezConsole
 {
@@ -6,7 +7,9 @@ namespace XadrezConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p = new Posicao(3, 4);
+            Console.WriteLine(p);
+            //Console.WriteLine("Hello World!");
         }
     }
 }
---
+++ XadrezConsole/Tabuleiro/Posicao.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace XadrezConsole.Tabuleiro
+{
+    class Posicao
+    {
+        public int Linha { get; set; }
+        public int Coluna { get; set; }
+
+        public Posicao(int linha, int coluna)
+        {
+            Linha = linha;
+            Coluna = coluna;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            builder.Append(Linha).Append(", ").Append(Coluna);
+            return builder.ToString();
+        }
+    }
+}
